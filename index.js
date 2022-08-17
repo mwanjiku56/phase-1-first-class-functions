63c7d9fd1fbe8981e3f5c85ad7fadac0ae41a94b
mercy wanjiku maruga <mercy.maruga@student.moringaschool.com>
2022-08-17 23:48:34 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback){ 
+    callback ();}
+
+function returnsANamedFunction(){
+    return function named(){}}
+
+function returnsAnAnonymousFunction(){
+    return function (){};
+}
